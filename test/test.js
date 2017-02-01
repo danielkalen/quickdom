6aee296a61ae6283c6479b260ded12b811b73eab
danielkalen <d@danielkalen.com>
2017-02-01 15:43:32 -0500

    [Build]

--- test/test.js
+++ test/test.js
@@ -47,7 +47,7 @@ suite("QuickDom", function() {
     packageVersion = (function(){
 			return {
 			  "name": "quickdom",
-			  "version": "1.0.6",
+			  "version": "1.0.7",
 			  "description": "Fast & light DOM element management supporting jquery-like methods, templates, & state-based styling",
 			  "main": "dist/quickdom.js",
 			  "browser": {
