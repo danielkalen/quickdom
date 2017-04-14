15ad3adbdb75b15b2787f504cb61e3e2ec0b6557
danielkalen <d@danielkalen.com>
2017-04-14 12:06:22 -0400

    [Build]

--- test/test.js
+++ test/test.js
@@ -101,7 +101,7 @@ var slice = [].slice;
         packageVersion = (function(){
 				return {
 				  "name": "quickdom",
-				  "version": "1.0.24",
+				  "version": "1.0.25",
 				  "description": "Fast & light DOM element management supporting jquery-like methods, templates, & state-based styling",
 				  "main": "dist/quickdom.js",
 				  "browser": {
