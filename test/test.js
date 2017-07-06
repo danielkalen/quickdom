7dbf63c3fc857c2eb601e201dc2897a085874b71
danielkalen <d@danielkalen.com>
2017-07-05 20:42:27 -0400

    [Build]

--- test/test.js
+++ test/test.js
@@ -125,7 +125,7 @@ suite("QuickDom", function() {
   setup(restartSandbox);
   test("Version Property", function() {
     var packageVersion;
-    packageVersion = "1.0.44";
+    packageVersion = "1.0.45";
     return expect(Dom.version).to.equal(packageVersion);
   });
   suite("Element Creation", function() {
